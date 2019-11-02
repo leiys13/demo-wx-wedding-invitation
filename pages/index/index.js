d6286f2575ab878e73fdf3454082b91d18aed1b0
leiys13 <leiys13@icloud.com>
2019-11-03 02:33:08 +0800

    实现背景音乐的播放暂停

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,32 @@
 Page({
   data: {
-    
+    isPlayingMusic: false
   },
+
+  bgm: null,
+  music_url: 'http://tally.sadasen.com/music/wuwenxidong_wf.mp3',
+  music_coverImgUrl: '/images/music-played.png',
   
-  onLoad: function () {
-    
+  onReady: function () {
+    this.bgm = wx.getBackgroundAudioManager()
+    this.bgm.title = 'marry me'
+    this.bgm.epname = 'wedding',
+    this.bgm.singer = 'singer'
+    this.bgm.coverImgUrl = this.music_coverImgUrl
+    this.bgm.onCanplay(()=> {
+      this.bgm.pause()
+    })
+    this.bgm.src = this.music_url
+  },
+
+  play: function(e) {
+    if(this.data.isPlayingMusic) {
+      this.bgm.pause()
+    } else {
+      this.bgm.play()
+    }
+    this.setData({
+      isPlayingMusic: !this.data.isPlayingMusic
+    })
   }
 })
