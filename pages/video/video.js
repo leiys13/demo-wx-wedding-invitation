ea8b45e9f9a15635962036c369d6a65fb4e87239
leiys13 <leiys13@icloud.com>
2019-11-05 00:23:19 +0800

    美好时光模块、视频

--- pages/video/video.js
+++ pages/video/video.js
@@ -5,62 +5,52 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    videoList: [{
+      createTime: '2019-08-10 20:56',
+      title: '林俊杰演唱会 - 珠海1',
+      src: '/images/IMG_6515.MP4',
+      danmuList: [{
+        text: '可惜没如果',
+        color: '#ff0000',
+        time: 1
+      }, {
+        text: '这妹纸是谁啊，好漂亮啊',
+        color: '#ff00ff',
+        time: 7
+      }, {
+        text: '妹纸唱歌真好听',
+        color: '#ff00ff',
+        time: 10
+      }, {
+        text: '好可爱de妹纸',
+        color: '#ff00ff',
+        time: 12
+      }]
+    }, {
+      createTime: '2019-08-10 20:58',
+      title: '林俊杰演唱会 - 珠海2',
+      src: '/images/IMG_6517.MP4'
+    }]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
+  videoContext: null,
+  inputValue: '',
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    this.videoContext = wx.createVideoContext('myVideo')
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  bindInputBlur: function(e) {
+    this.inputValue = e.detail.value
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  bindSendDanmu: function() {
+    this.videoContext.sendDanmu({
+      text: this.inputValue,
+      color: '#f90'
+    })
   }
 })
