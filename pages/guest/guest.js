76733116b663b143780649b576f3f86af20d326d
leiys13 <leiys13@icloud.com>
2019-11-07 00:53:33 +0800

    宾客信息模块

--- pages/guest/guest.js
+++ pages/guest/guest.js
@@ -5,62 +5,58 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    picker: {
+      arr: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '更多'],
+      index: 1
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  pickerChange: function(e) {
+    this.setData({
+      'picker.index': e.detail.value
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  nameChange: function(e) {
+    this.checkName(e.detail.value)
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  phoneChange: function(e) {
+    this.checkPhone(e.detail.value)
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  checkName: function(data) {
+    var reg = /^[\u4E00-\u9FA5A-Za-z]+$/
+    return this.check(data, reg, '姓名输入错误')
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  checkPhone: function(data) {
+    var reg = /^(((13)|(15)|(17)|(18))\d{9})$/
+    return this.check(data, reg, '手机号输入错误')
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  formSubmit: function(e) {
+    var name = e.detail.value.name
+    var phone = e.detail.value.phone
+    if(this.checkName(name) && this.checkPhone(phone)) {
+      wx.showToast({
+        title: '提交成功',
+        icon: 'success',
+        duration: 1500
+      })
+    }
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  check: function(data, reg, errMsg) {
+    if(!reg.test(data)) {
+      wx.showToast({
+        title: errMsg,
+        icon: 'none',
+        duration: 1500
+      })
+      return false
+    }
+    return true
   }
+
 })
